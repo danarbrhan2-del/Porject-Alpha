81cc6cdc48fae9e72d8159d518fcf894a5063592
danarbrhan2-del <danarbrhan2@gmail.com>
2026-02-27 18:58:46 +0300

    Add files via upload

---
+++ Porject Alfa/main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+
+public class main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+}
